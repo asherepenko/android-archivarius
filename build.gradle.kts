cc1ac31a4b3e7e1b3fc3092210163a6e05d6dea0
Andrew Sherepenko <andrew.sherepenko@gmail.com>
2020-03-05 18:54:28 +0200

    Update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.0")
-        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.70"))
     }
 }
 
